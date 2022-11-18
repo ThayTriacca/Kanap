b1b20cf1d13668d66b03775c992cf02121868e95
ThayTriacca <thay_triacca@hotmail.com>
2022-11-18 00:39:12 +0000

    changequantity function

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,22 +1,18 @@
-
-//Display products added on cart
+//--------------Display products added on cart------------------
 let myCart = JSON.parse(localStorage.getItem('cart'));
 console.log(myCart);
 
+
 let buyItems = [];
 let cartSection = document.getElementById('cart__items');
-// function ola(){
-  //   console.log(myCart[1].identifier);
-  // }
-  for (let product in myCart) {
-    let productArticle = document.createElement('a');
-    productArticle.classList.add('cart__item');
-    productArticle.setAttribute('data-identifier', '{myCart[product].identifier}');
-    
-    buyItems.push(myCart[product].productId);
-    // console.log(buyItems);
-    
-    productArticle.innerHTML =
+for (let product in myCart) {
+  let productArticle = document.createElement('a');
+  productArticle.classList.add('cart__item');
+  productArticle.setAttribute('data-identifier', `${myCart[product].identifier}`);
+  
+  buyItems.push(myCart[product].productId);
+  
+  productArticle.innerHTML =
     `<div class="cart__item__img">
     <img src="${myCart[product].productImg}" alt="${myCart[product].imgAlt}">
     </div>
@@ -36,146 +32,159 @@ let cartSection = document.getElementById('cart__items');
     </div>
     </div>
     </div>`
-    
-    cartSection.appendChild(productArticle);
-  }
-  
-  // Update Cart
 
-  function updateCart(){
+  cartSection.appendChild(productArticle);
+}
 
+
+//-------------------Update Cart---------------------------
+
+
+function updateCart() {
+  let myCart = JSON.parse(localStorage.getItem('cart'))
+  let qtyTotal = 0;
+  let totalPrice = 0;
+  for (i = 0; i < myCart.length; i++) {
+    qtyTotal += Number(myCart[i].quantityItems);
+    totalPrice += myCart[i].price * myCart[i].quantityItems;
   }
-  //Sum of prices
-  
-  function getTotalPrice(myCart){
-    let total = 0;
-    for (let i=0; i < myCart.length; i++){
-      total += myCart[i].price * myCart[i].quantityItems;
-    }
-    return total.toFixed(2);
+  document.getElementById('totalPrice').innerHTML = totalPrice;
+  document.getElementById('totalQuantity').innerHTML = qtyTotal;
+};
+
+updateCart();
+
+
+//---------------Delete products------------------
+
+function deleteItemCart() {
+
+  let deleteBtn = document.getElementsByClassName('deleteItem');
+
+  for (let i = 0; i < deleteBtn.length; i++) {
+    let deleteItem = deleteBtn[i];
+    deleteItem.addEventListener('click', function (event) {
+      itemSelected = event.target;
+      let productIdentifier = itemSelected.closest('.cart__item').getAttribute('data-identifier');
+      itemSelected.closest('.cart__item').remove();
+      let productDeleted = myCart.find(cart => cart.identifier == productIdentifier);
+      if (productDeleted) {
+        let index = myCart.indexOf(productDeleted);
+        myCart.splice(index, 1);
+        localStorage.setItem('cart', JSON.stringify(myCart));
+        updateCart();
+      }
+    })
   }
-  
-  //Sum of quantities
-  function getTotalQuantity(myCart){
-    let totalQty = 0;
-    for (let i=0; i< myCart.length; i++) {
+  localStorage.setItem('cart', JSON.stringify(myCart));
+};
+
+deleteItemCart();
+
+
+//-----------------Change Quantity------------
+//event target closest 
+function changeQuantity() {
+  let quantityItems = document.querySelectorAll(".itemQuantity");
+
+  for (let i = 0; i < quantityItems.length; i++) {
+    quantityItems[i].addEventListener("change", (event) => {
+      event.preventDefault();
+
+      myCart[i].quantityItems = quantityItems[i].valueAsNumber;
+      window.localStorage.setItem("cart", JSON.stringify(myCart));
       
-      totalQty += Number(myCart[i].quantityItems);
-    }
-    return totalQty;
+      updateCart();
+    })
   }
-  
-  //Update Cart Price and Quantity DOM
-  let qtyTotal = document.getElementById('totalQuantity');
-  let totalPrice = document.getElementById('totalPrice');
-  qtyTotal.innerHTML = getTotalQuantity(myCart);
-  totalPrice.innerHTML = getTotalPrice(myCart);
+};
+
+changeQuantity();
 
 
-  //Delete products
+//---------------Finish Order----------
 
-  function deleteItemCart () {
 
-    let deleteBtn = document.getElementsByClassName('deleteItem');
 
-    for ( let i = 0; i <  deleteBtn.length; i ++) {
-      let deleteItem = deleteBtn[i];
-      deleteItem.addEventListener('click', function (event) {
-        itemSelected = event.target;
-        let productIdentifier = itemSelected.closest('.cart__item').getAttribute('data-identifier');
-        itemSelected.closest('.cart__item').remove();
-        let productDeleted = myCart.find(cart => cart.id == productIdentifier);
-        if (productDeleted !== -1) {
-          myCart.splice(productDeleted, 1);
-          getTotalPrice(myCart);
-          getTotalQuantity(myCart);
-          localStorage.setItem('cart', JSON.stringify (myCart));
-        }
-      })
-    }
-    localStorage.setItem('cart', JSON.stringify(myCart));
-  };
 
-  deleteItemCart();
+//---------------FORM--------------
 
-    //Change Quantity
-    //onchange
-    
-    //Finish Order
-    
-    //FORM
-    
-    //Access DOM Elements
-    
-    const firstNameInput = document.getElementById('firstName');
-    const firstNameError = document.getElementById('firstNameErrorMsg');
-    const lastNameInput = document.getElementById('lastName');
-    const lastNameError = document.getElementById('lastNameErrorMsg');
-    const addressInput = document.getElementById('address');
-    const addressError = document.getElementById('addressErrorMsg');
-    const cityInput = document.getElementById('city');
-    const cityError = document.getElementById('cityErrorMsg');
-    const emailInput = document.getElementById('email');
-    const emailError = document.getElementById('emailErrorMsg');
-    const submitButton = document.getElementById('order');
-    const form = document.getElementsByClassName('cart__order__form')[0];
+//------------Access DOM Elements-----------
 
-     //Function add error
-    function addError(input) {
-      return 'thin solid ' + (input != '' ? 'green' : 'red');
-    };
-    //First Name Form
-    
-    firstNameInput.addEventListener('blur', (event) => {
-      const input = event.target.value;
-      firstNameInput.style.border = addError(input);
-      input == '' && (firstNameError.innerHTML = firstNameInput.validationMessage);
+const firstNameInput = document.getElementById('firstName');
+const firstNameError = document.getElementById('firstNameErrorMsg');
+const lastNameInput = document.getElementById('lastName');
+const lastNameError = document.getElementById('lastNameErrorMsg');
+const addressInput = document.getElementById('address');
+const addressError = document.getElementById('addressErrorMsg');
+const cityInput = document.getElementById('city');
+const cityError = document.getElementById('cityErrorMsg');
+const emailInput = document.getElementById('email');
+const emailError = document.getElementById('emailErrorMsg');
+const submitButton = document.getElementById('order');
+const form = document.getElementsByClassName('cart__order__form')[0];
+
+//---------------Function add error------------
+
+function addError(input) {
+  return 'thin solid ' + (input != '' ? 'green' : 'red');
+};
+
+
+//----------------First Name Form----------------
+
+firstNameInput.addEventListener('blur', (event) => {
+  const input = event.target.value;
+  firstNameInput.style.border = addError(input);
+  input == '' && (firstNameError.innerHTML = firstNameInput.validationMessage);
 });
 
-//Last Name Form
+
+//------------------Last Name Form-------------------
 
 lastNameInput.addEventListener('blur', (event) => {
-    const input = event.target.value;
-    lastNameInput.style.border = addError(input);
-    input == '' && (lastNameError.innerHTML = lastNameInput.validationMessage);
+  const input = event.target.value;
+  lastNameInput.style.border = addError(input);
+  input == '' && (lastNameError.innerHTML = lastNameInput.validationMessage);
 });
 
-//Address Form
+
+//-------------------Address Form-------------
 
 addressInput.addEventListener('blur', (event) => {
-    const input = event.target.value;
-    addressInput.style.border = addError(input);
-    input == '' && (addressError.innerHTML = addressInput.validationMessage);
+  const input = event.target.value;
+  addressInput.style.border = addError(input);
+  input == '' && (addressError.innerHTML = addressInput.validationMessage);
 });
 
-//City Form
+
+//------------------City Form--------------
 
 cityInput.addEventListener('blur', (event) => {
-    const input = event.target.value;
-    cityInput.style.border = addError(input);
-    input == '' && (cityError.innerHTML = cityInput.validationMessage);
+  const input = event.target.value;
+  cityInput.style.border = addError(input);
+  input == '' && (cityError.innerHTML = cityInput.validationMessage);
 });
 
-//Email Form
+
+//-----------------Email Form--------------
 
 emailInput.addEventListener('blur', (event) => {
-    const input = event.target.value;
-    emailInput.style.border = addError(input);
-    input == '' && (emailError.innerHTML = emailInput.validationMessage);
+  const input = event.target.value;
+  emailInput.style.border = addError(input);
+  input == '' && (emailError.innerHTML = emailInput.validationMessage);
 });
 
-//Submit Event Listener
+
+//--------------Submit Event Listener------------
 
 submitButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    const validate = form.checkValidity();
-    form.reportValidity();
-    if (validate) {
-        //enviar para backend -order
-
-        //reset
-        form.reset();
-        return;
-    }
-    emailError.innerHTML = emailInput.validationMessage;
+  event.preventDefault();
+  const validate = form.checkValidity();
+  form.reportValidity();
+  if (validate) {
+    form.reset();
+    return;
+  }
+  emailError.innerHTML = emailInput.validationMessage;
 });
